671f069b1f6b5ba6681b043812e7f7923e37c3f4
Cody Winton <cody.t.winton@gmail.com>
2018-05-21 17:31:49 -0500

    Updated GitHub for better usage

--- _includes/sections/github.js
+++ _includes/sections/github.js
@@ -1,90 +1,72 @@
-(function() {
-  let repos = {{ include.repos | jsonify }};
+;(function() {
+  let repos = JSON.parse(`{{ include.repos | jsonify }}`)
 
   for (let i = 0; i < repos.length; i++) {
-    let item = repos[i];
+    let item = repos[i]
 
     requestRepo(item.owner, item.repo)
       .then(function(response) {
-        let linkId = item.repo + "-link";
-        document.getElementById(linkId).href = response.html_url;
-
-        let descId = item.repo + "-description";
-        document.getElementById(descId).innerHTML = response.description;
-
-        //document.getElementById(item.repo + '-language').innerHTML = 'Built in ' + response.language + ' on GitHub';
+        let descId = `${item.repo}-description`
+        document.getElementById(descId).innerHTML = response.description
+        document.getElementById(`${item.repo}-link`).href = response.html_url
       })
       .catch(function(err) {
-        console.error(err);
-      });
+        console.error(err)
+      })
 
     requestRepoLanguages(item.owner, item.repo)
       .then(function(response) {
-        let langId = item.repo + "-languages";
-        let langEl = document.getElementById(langId);
-
-        let innerData = "";
-
-        for (let k = 0; k < response.length; k++) {
-          innerData = innerData + "<span class=\"badge badge-secondary\">" + response[k] + "</span>\n";
-        }
-
-        langEl.innerHTML = innerData;
+        let langEl = document.getElementById(`${item.repo}-languages`)
+        langEl.innerHTML = response
+          .map(function(lang) {
+            return `<span class="badge badge-secondary">${lang}</span>`
+          })
+          .join('\n')
       })
       .catch(function(err) {
-        console.error(err);
-      });
+        console.error(err)
+      })
   }
-})();
+})()
 
 function requestRepo(owner, repo) {
   return new Promise(function(resolve, reject) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.github.com/repos/" + owner + "/" + repo, true);
-    xhr.setRequestHeader("Content-type", "application/json");
+    let xhr = new XMLHttpRequest()
+    let url = `https://api.github.com/repos/${owner}/${repo}`
+    xhr.open('GET', url, true)
+    xhr.setRequestHeader('Content-type', 'application/json')
     xhr.onload = function(event) {
-      if (
-        this.readyState < 4 ||
-        this.responseText == null ||
-        this.responseText == undefined
-      ) {
-        reject({ request: xhr });
+      if (this.readyState < 4) {
+        reject({ request: xhr })
       } else {
-        let response = JSON.parse(this.responseText);
-        resolve(response);
+        let response = JSON.parse(this.responseText)
+        resolve(response)
       }
-    };
+    }
     xhr.onerror = function(error) {
-      reject(error);
-    };
-    xhr.send();
-  });
+      reject(error)
+    }
+    xhr.send()
+  })
 }
 
 function requestRepoLanguages(owner, repo) {
   return new Promise(function(resolve, reject) {
-    let xhr = new XMLHttpRequest();
-    xhr.open(
-      "GET",
-      "https://api.github.com/repos/" + owner + "/" + repo + "/languages",
-      true
-    );
-    xhr.setRequestHeader("Content-type", "application/json");
+    let xhr = new XMLHttpRequest()
+    let url = `https://api.github.com/repos/${owner}/${repo}/languages`
+    xhr.open('GET', url, true)
+    xhr.setRequestHeader('Content-type', 'application/json')
     xhr.onload = function(event) {
-      if (
-        this.readyState < 4 ||
-        this.responseText == null ||
-        this.responseText == undefined
-      ) {
-        reject({ request: xhr });
+      if (this.readyState < 4) {
+        reject({ request: xhr })
       } else {
-        let response = JSON.parse(this.responseText);
-        resolve(Object.keys(response));
+        let response = JSON.parse(this.responseText)
+        resolve(Object.keys(response))
       }
-    };
+    }
     xhr.onerror = function(error) {
-      reject(error);
-    };
-    xhr.send();
-  });
+      reject(error)
+    }
+    xhr.send()
+  })
 }
