e445df784d7336f963774ee378e59d9af142780c
Cody Winton <cody.t.winton@gmail.com>
2018-05-21 17:10:19 -0500

    Updated theme to show GitHub languages

--- _includes/sections/github.js
+++ _includes/sections/github.js
@@ -1,34 +1,90 @@
-(function () {
-  let repos = JSON.parse('{{ include.repos | jsonify }}');
+(function() {
+  let repos = {{ include.repos | jsonify }};
 
   for (let i = 0; i < repos.length; i++) {
     let item = repos[i];
-    let repoAPIURL = 'https://api.github.com/repos/' + item.owner + '/' + item.repo;
 
-    let xhttp = new XMLHttpRequest();
-    xhttp.open('GET', repoAPIURL, true);
-    xhttp.setRequestHeader('Content-type', 'application/json');
-    xhttp.onreadystatechange = function (event) {
+    requestRepo(item.owner, item.repo)
+      .then(function(response) {
+        let linkId = item.repo + "-link";
+        document.getElementById(linkId).href = response.html_url;
 
-      if (this.readyState < 4) {
-        return;
-      }
-      if (!this.responseText) {
-        console.error('No response text');
-        return;
-      }
+        let descId = item.repo + "-description";
+        document.getElementById(descId).innerHTML = response.description;
 
-      let response = JSON.parse(this.responseText);
+        //document.getElementById(item.repo + '-language').innerHTML = 'Built in ' + response.language + ' on GitHub';
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
 
-      document.getElementById(item.repo + '-link').href = response.html_url;
-      document.getElementById(item.repo + '-description').innerHTML = response.description;
-      document.getElementById(item.repo + '-language').innerHTML = 'Built in ' + response.language + ' on GitHub';
-    };
+    requestRepoLanguages(item.owner, item.repo)
+      .then(function(response) {
+        let langId = item.repo + "-languages";
+        let langEl = document.getElementById(langId);
 
-    xhttp.onerror = function (error) {
-      console.error(error);
-    }
+        let innerData = "";
 
-    xhttp.send();
+        for (let k = 0; k < response.length; k++) {
+          innerData = innerData + "<span class=\"badge badge-secondary\">" + response[k] + "</span>\n";
+        }
+
+        langEl.innerHTML = innerData;
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
   }
 })();
+
+function requestRepo(owner, repo) {
+  return new Promise(function(resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.github.com/repos/" + owner + "/" + repo, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onload = function(event) {
+      if (
+        this.readyState < 4 ||
+        this.responseText == null ||
+        this.responseText == undefined
+      ) {
+        reject({ request: xhr });
+      } else {
+        let response = JSON.parse(this.responseText);
+        resolve(response);
+      }
+    };
+    xhr.onerror = function(error) {
+      reject(error);
+    };
+    xhr.send();
+  });
+}
+
+function requestRepoLanguages(owner, repo) {
+  return new Promise(function(resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(
+      "GET",
+      "https://api.github.com/repos/" + owner + "/" + repo + "/languages",
+      true
+    );
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onload = function(event) {
+      if (
+        this.readyState < 4 ||
+        this.responseText == null ||
+        this.responseText == undefined
+      ) {
+        reject({ request: xhr });
+      } else {
+        let response = JSON.parse(this.responseText);
+        resolve(Object.keys(response));
+      }
+    };
+    xhr.onerror = function(error) {
+      reject(error);
+    };
+    xhr.send();
+  });
+}
